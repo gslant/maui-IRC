c8431b7542fe66cb4e13a4672faaeeb870bbfcd0
gslant <77141272+gslant@users.noreply.github.com>
2024-10-02 14:21:25 +1300

    Extract Channel class to Models

---
+++ IRC/Models/Channel.cs
@@ -0,0 +1,47 @@
+﻿using IRC.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+public class Channel : INotifyPropertyChanged
+{
+    public string Name { get; set; }
+    private ObservableCollection<Message> _messages;
+    public ObservableCollection<Message> Messages
+    {
+        get => _messages;
+        set
+        {
+            _messages = value;
+            OnPropertyChanged(nameof(Messages));
+        }
+    }
+
+    public Channel(string name)
+    {
+        Name = name;
+        Messages = new ObservableCollection<Message>();
+    }
+
+    public void AddMessage(Message m)
+    {
+        Messages.Add(m);
+        OnPropertyChanged(nameof(Messages));
+    }
+
+    public static Channel? GetChannelByName(ObservableCollection<Channel> channels, string name)
+    {
+        return channels.FirstOrDefault(c => c.Name == name);
+    }
+
+    public static bool ContainsChannelByName(ObservableCollection<Channel> channels, string name)
+    {
+        return channels.Any(c => c.Name == name);
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
